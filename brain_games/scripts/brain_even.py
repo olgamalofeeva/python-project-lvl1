b6ee9b433978da6afc0412b087d4abb6a880b16d
Olga <oli.malofeeva@mail.ru>
2021-02-09 01:27:49 +0300

    update brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import prompt
+from random import randint
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+
+greet()
+
+name = prompt.string('May I have your name? ')
+
+
+def welcome_user():
+    print('Hello, ' + name + '!')
+
+welcome_user()
+
+print('Answer "yes" if the number is even, otherwise answer "no".')
+
+def game_description():
+  i = 1
+
+  while i <= 3:
+    random_number = randint(1, 100)
+    print('Question: ' + str(random_number))
+    user_answer = input("Your answer: ")
+    
+    if random_number % 2 == 0 and user_answer == 'yes' or random_number % 2 != 0 and user_answer == 'no':
+        print('Correct!') 
+
+    else: 
+        print("'{}' is wrong answer. Correct answer was '{}'.\nLet's try again, {}!".format(user_answer, , name))
+        exit()
+
+    i += 1
+  print('Congratulations,{}!'.format(name))
+
+def main():
+    game_description()
+
+
+if __name__ == '__main__':
+    main()
