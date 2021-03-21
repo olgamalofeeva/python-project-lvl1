75de168cd35220a3b677a5dd3b9325497086b480
Olga <oli.malofeeva@mail.ru>
2021-03-22 01:03:08 +0300

    fff

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -13,7 +13,7 @@ def counts():
     while i <= 3:
         START = random.randint(1, 10)
         STEP = random.randint(1, 5)
-        LIMIT = random.randint(6, 11)
+        LIMIT = 10
         MISSED_NUMBER = random.randint(1, 5)
 
         RANGE_NUMBERS = [i for i in range(START, START + STEP * LIMIT, STEP)]
