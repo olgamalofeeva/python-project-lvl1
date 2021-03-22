adff65da177c0bc8a8218d22b3e14d3ef0295e07
Olga <oli.malofeeva@mail.ru>
2021-03-22 22:48:54 +0300

    back changes

---
+++ brain_games/scripts/games/brain_games.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import prompt
+
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+
+greet()
+
+
+name = prompt.string('May I have your name? ')
+
+
+def welcome_user():
+    print('Hello, ' + name + '!')
+
+
+def main():
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
